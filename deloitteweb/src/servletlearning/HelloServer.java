769b99eb6500b83f90c405968c51b5e6b0941bc5
aishwarya234 <45032806+aishwarya234@users.noreply.github.com>
2020-01-04 17:31:12 +0530

    Add files via upload

---
+++ deloitteweb/src/servletlearning/HelloServer.java
@@ -0,0 +1,31 @@
+package servletlearning;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HelloServer
+ */
+public class HelloServer extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		
+		response.setContentType("text/html");
+		PrintWriter out=response.getWriter();
+		out.println("Hello SERver <br>");
+		out.println("<h2>Hello Server</h2>");
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+}
